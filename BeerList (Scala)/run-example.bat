859abb121c2fd6ca732d1a78bee40eefcd20459f
Marko Elezovic <marko@element.hr>
2014-12-15 18:30:23 +0100

    Reduced memory requirements for the Scala example. Added binary outputs for build systems to gitignore.

--- BeerList (Scala)/run-example.bat
+++ BeerList (Scala)/run-example.bat
@@ -5,9 +5,9 @@ rem -XX:MaxPermSize JVM option is not required if you are running on JVM 8+
 rem input.encoding is neccessary to fix jline problems in SBT shell
 
 java ^
-  -XX:MaxPermSize=256m ^
+  -XX:MaxPermSize=128m ^
   -Dinput.encoding=Cp1252 ^
-  -Xmx2g ^
+  -Xmx640m ^
   -jar project\sbt-launch-0.13.7.jar ^
   run
 
--- BeerList (Scala)/run-example.sh
+++ BeerList (Scala)/run-example.sh
@@ -4,7 +4,7 @@ cd "$( dirname "$0" )"
 # -XX:MaxPermSize JVM option is not required if you are running on JVM 8+
 
 exec java \
-  -XX:MaxPermSize=256m \
-  -Xmx2g \
+  -XX:MaxPermSize=128m \
+  -Xmx640m \
   -jar project/sbt-launch-0.13.7.jar \
   run
