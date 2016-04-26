88746c717ccc7e8b49d095997a7a27fa6985b5d9
Rikard Pavelic <rikard@ngs.hr>
2016-04-26 16:20:07 +0200

    .NET example showing [[equals]] helper tag in Excel
    equals tag is used for conversion from text into formula.

---
+++ Beginner/ToFormulaConversion (.NET)/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+using System.IO;
+using NGS.Templater;
+
+namespace ChartExample
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			File.Copy("SimpleConversion.xlsx", "out.xlsx", true);
+
+			using (var doc = Configuration.Factory.Open("out.xlsx"))
+			{
+				doc.Process(new { aa = 100, bb = 22.2m });
+			}
+			Process.Start("out.xlsx");
+		}
+	}
+}
