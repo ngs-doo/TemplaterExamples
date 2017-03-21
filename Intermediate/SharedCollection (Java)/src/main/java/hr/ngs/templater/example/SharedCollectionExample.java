78bbca01795f27d8e7f33a087a0158d3522202ad
Rikard Pavelic <rikard@ngs.hr>
2017-03-21 17:10:13 +0100

    Allow setting custom DPI through plugin

--- Intermediate/SharedCollection (Java)/src/main/java/hr/ngs/templater/example/SharedCollectionExample.java
+++ Intermediate/SharedCollection (Java)/src/main/java/hr/ngs/templater/example/SharedCollectionExample.java
@@ -1,9 +1,15 @@
 package hr.ngs.templater.example;
 
 import hr.ngs.templater.*;
+import org.w3c.dom.NodeList;
 
-import javax.imageio.ImageIO;
+import javax.imageio.*;
+import javax.imageio.metadata.IIOMetadata;
+import javax.imageio.metadata.IIOMetadataNode;
+import javax.imageio.stream.ImageInputStream;
+import javax.imageio.stream.ImageOutputStream;
 import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.*;
 import java.util.*;
 import java.util.List;
@@ -46,11 +52,70 @@ public class SharedCollectionExample {
 		}
 	}
 
+	static class ImageWithDPI implements IDocumentFactoryBuilder.IFormatter {
+
+		@Override
+		public Object format(Object value, String md) {
+			if (md.startsWith("dpi(") && value instanceof String) {
+				try {
+					int dpi = Integer.parseInt(md.substring("dpi(".length(), md.length() - 1));
+					String resource = (String) value;
+					InputStream is = ImageWithDPI.class.getResourceAsStream(resource);
+					String ext = resource.substring(resource.lastIndexOf('.'));
+					File file = File.createTempFile("picture", ext);
+					FileOutputStream fos = new FileOutputStream(file);
+					byte[] buf = new byte[4096];
+					int read;
+					while ((read = is.read(buf)) != -1) {
+						fos.write(buf, 0, read);
+					}
+					fos.close();
+					BufferedImage bufImg = ImageIO.read(file);
+					ImageInputStream iis = ImageIO.createImageInputStream(file);
+					for (Iterator<ImageReader> ir = ImageIO.getImageReaders(iis); ir.hasNext(); ) {
+						ImageWriter writer = ImageIO.getImageWriter(ir.next());
+						ImageWriteParam writeParam = writer.getDefaultWriteParam();
+						ImageTypeSpecifier typeSpecifier = ImageTypeSpecifier.createFromBufferedImageType(BufferedImage.TYPE_INT_RGB);
+						IIOMetadata metadata = writer.getDefaultImageMetadata(typeSpecifier, writeParam);
+						if (metadata.isReadOnly() || !metadata.isStandardMetadataFormatSupported()) {
+							continue;
+						}
+
+						double dotsPerMilli = 1.0 * dpi / 10 / 2.54;
+						IIOMetadataNode horiz = new IIOMetadataNode("HorizontalPixelSize");
+						horiz.setAttribute("value", Double.toString(dotsPerMilli));
+						IIOMetadataNode vert = new IIOMetadataNode("VerticalPixelSize");
+						vert.setAttribute("value", Double.toString(dotsPerMilli));
+
+						IIOMetadataNode dim = new IIOMetadataNode("Dimension");
+						dim.appendChild(horiz);
+						dim.appendChild(vert);
+
+						IIOMetadataNode root = new IIOMetadataNode("javax_imageio_1.0");
+						root.appendChild(dim);
+
+						metadata.mergeTree("javax_imageio_1.0", root);
+
+						final ImageOutputStream stream = ImageIO.createImageOutputStream(file);
+						writer.setOutput(stream);
+						writer.write(metadata, new IIOImage(bufImg, null, metadata), writeParam);
+						stream.close();
+						break;
+					}
+					return ImageIO.createImageInputStream(file);
+				} catch (IOException e) {
+					throw new RuntimeException(e);
+				}
+			}
+			return value;
+		}
+	}
+
 	public static void main(final String[] args) throws Exception {
 		InputStream templateStream = SharedCollectionExample.class.getResourceAsStream("/TwoTables.docx");
 		File tmp = File.createTempFile("collection", ".docx");
 		FileOutputStream fos = new FileOutputStream(tmp);
-		IDocumentFactory factory = Configuration.factory();
+		IDocumentFactory factory = Configuration.builder().include(new ImageWithDPI()).build();
 		ITemplateDocument tpl = factory.open(templateStream, "docx", fos);
 		HashMap<String, Object> data = new HashMap<String, Object>();
 		data.put("analysis", "Patient info");
@@ -66,16 +131,7 @@ public class SharedCollectionExample {
 				Arrays.asList(new History("Claw cut", 1), new History("Bruising", 0)),
 				Arrays.asList(new Medicine("Vitamins", 4, 8, 365), new Medicine("Fiber", 6, 8, 365))));
 
-		InputStream is = SharedCollectionExample.class.getResourceAsStream("/java.jpg");
-		File tmpPath = File.createTempFile("picture", "jpg");
-		FileOutputStream tmpFos = new FileOutputStream(tmpPath);
-		byte[] buf = new byte[4096];
-		int read;
-		while ((read = is.read(buf)) != -1) {
-			tmpFos.write(buf, 0, read);
-		}
-		tmpFos.close();
-		data.put("imageWithDPI", ImageIO.createImageInputStream(tmpPath));
+		data.put("imageWithDPI", "/java.png");
 
 		tpl.process(data);
 		tpl.flush();
